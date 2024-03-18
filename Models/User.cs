c0898672fc6ddafd8610f82c811a309013baa960
upgradedtofml <ckan1995@hotmail.com>
2024-03-18 12:48:57 +0200

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+namespace IndividualNorthwindEshop.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public Role Role { get; set; }
+        public virtual Customer Customer { get; set; }
+        public virtual Employee Employee { get; set; }
+    }
+    public enum Role
+    {
+        Customer,
+        Admin,
+        Manager
+    }
+}
